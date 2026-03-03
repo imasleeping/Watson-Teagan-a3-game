deaacaa52dced453678765b22f781b3bbb066013
GeorgeGimme <144726058+imasleeping@users.noreply.github.com>
2026-03-03 17:43:13 -0500

    Split code into classes

--- Game.cs
+++ Game.cs
@@ -1,5 +1,7 @@
 ﻿// Include the namespaces (code libraries) you need below.
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
 
 // The namespace your code is in.
@@ -10,24 +12,83 @@ namespace MohawkGame2D
     /// </summary>
     public class Game
     {
-        // Place your variables here:
 
-
-        /// <summary>
-        ///     Setup runs once before the game loop begins.
-        /// </summary>
+        Objects objects = new Objects();
+        Vector2 Velocity = new Vector2(0, 0);
+        Vector2 Position = new Vector2(100, 100);
+        int Size = 50;
+        int Speed = 50;
+        int JumpForce = 400;
+        int Gravity = 10;
         public void Setup()
         {
-
+            Window.SetSize(1400, 1000);
         }
-
-        /// <summary>
-        ///     Update runs every frame.
-        /// </summary>
         public void Update()
         {
 
+            Window.ClearBackground(Color.Black);
+
+            //player parameters
+            Draw.FillColor = Color.White;
+            Draw.Square(Position, Size);
+
+            //velocity to movement
+            Position += Velocity * Time.DeltaTime;
+
+            //left boundary
+            if (Position.X < 0)
+            {
+                //wall bouncyness left
+                Velocity.X += 50;
+            }
+            else
+            {
+                //left movement
+                if (Input.IsKeyboardKeyDown(KeyboardInput.A))
+                {
+                    Velocity.X -= Speed;
+                }
+            }
+            //right boundary
+            if (Position.X > Window.Size.X - Size)
+            {
+                //wall bouncyness right
+                Velocity.X -= 50;
+            }
+            else
+            {
+                //right movemnt
+                if (Input.IsKeyboardKeyDown(KeyboardInput.D))
+                {
+                    Velocity.X += Speed;
+                }
+            }
+            //floor boundary
+            if (Position.Y < Window.Size.Y - Size)
+            {
+                //gravity
+                Velocity.Y += Gravity;
+            }
+            else
+            {
+                //stop on floor
+                Velocity.Y = 0;
+                //jump
+                if (Input.IsKeyboardKeyPressed(KeyboardInput.W))
+                {
+                    Velocity.Y -= JumpForce;
+                }
+            }
+            //drag X axis
+            Velocity.X += (-Velocity.X * 0.2f);
+            
+            if (Input.IsMouseButtonPressed(0))
+            {
+                objects.AddBoxVelocity(new Vector2(0, 0));
+                objects.AddBoxPosition(Input.GetMousePosition());
+            }
+            objects.MoveAndDraw();
         }
     }
-
 }
---
+++ Objects.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace MohawkGame2D
+{
+
+public class Objects
+{
+        public List<Vector2> BoxPositions = new List<Vector2>();
+        public List<Vector2> BoxVelocitys = new List<Vector2>();
+        Vector2 CurrBoxVelocity = new Vector2(0, 0);
+        Vector2 CurrBoxPos = new Vector2(0, 0);
+        int Gravity = 10;
+        public void AddBoxVelocity(Vector2 Velocity)
+        {
+            BoxVelocitys.Add(Velocity);
+        }
+        public void AddBoxPosition(Vector2 Position)
+        {
+            BoxPositions.Add(Position);
+        }
+        public void MoveAndDraw()
+	{
+            for (int i = 0; i < BoxPositions.Count; i++)
+            {
+                Vector2 BoxPosition = BoxPositions[i];
+                CurrBoxVelocity = BoxVelocitys[i];
+                CurrBoxPos = BoxPositions[i];
+                if (CurrBoxPos.Y < Window.Size.Y - 50)
+                {
+                    BoxVelocitys[i] += new Vector2(0, Gravity);
+                }
+                else
+                {
+                    CurrBoxVelocity.Y = 0;
+                }
+                for (int I = 0; I < BoxPositions.Count; I++)
+                {
+                    if (CurrBoxPos.Y > BoxPositions[I].Y)
+                    {
+                        if (CurrBoxPos != BoxPositions[I])
+                        {
+                            CurrBoxVelocity = new Vector2(0, 0);
+                        }
+                    }
+                }
+                BoxPositions[i] += CurrBoxVelocity * Time.DeltaTime;
+                Draw.FillColor = Color.Red;
+                Draw.Square(BoxPositions[i], 50);
+            }
+        }
+}
+
+}
+
