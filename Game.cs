c445b56cf9356f39ff3d6c6f257f7f4d2aebde29
GeorgeGimme <144726058+imasleeping@users.noreply.github.com>
2026-03-10 17:17:43 -0400

    moved player code  to new class

--- Game.cs
+++ Game.cs
@@ -14,75 +14,14 @@ namespace MohawkGame2D
     {
 
         Objects objects = new Objects();
-        Vector2 Velocity = new Vector2(0, 0);
-        Vector2 Position = new Vector2(100, 100);
-        int Size = 25;
-        int Speed = 50;
-        int JumpForce = 400;
-        int Gravity = 10;
+        Player player = new Player();
         public void Setup()
         {
             Window.SetSize(800, 600);
         }
         public void Update()
         {
-
             Window.ClearBackground(Color.Black);
-
-            //player parameters
-            Draw.FillColor = Color.White;
-            Draw.Square(Position, Size);
-
-            //velocity to movement
-            Position += Velocity * Time.DeltaTime;
-
-            //left boundary
-            if (Position.X < 0)
-            {
-                //wall bouncyness left
-                Velocity.X += 50;
-            }
-            else
-            {
-                //left movement
-                if (Input.IsKeyboardKeyDown(KeyboardInput.A))
-                {
-                    Velocity.X -= Speed;
-                }
-            }
-            //right boundary
-            if (Position.X > Window.Size.X - Size)
-            {
-                //wall bouncyness right
-                Velocity.X -= 50;
-            }
-            else
-            {
-                //right movemnt
-                if (Input.IsKeyboardKeyDown(KeyboardInput.D))
-                {
-                    Velocity.X += Speed;
-                }
-            }
-            //floor boundary
-            if (Position.Y < Window.Size.Y - Size)
-            {
-                //gravity
-                Velocity.Y += Gravity;
-            }
-            else
-            {
-                //stop on floor
-                Velocity.Y = 0;
-                Position.Y = Window.Size.Y - Size;
-                //jump
-                if (Input.IsKeyboardKeyPressed(KeyboardInput.W))
-                {
-                    Velocity.Y -= JumpForce;
-                }
-            }
-            //drag X axis
-            Velocity.X += (-Velocity.X * 0.2f);
 
             //if mouse is pressed spawn a new box
             if (Input.IsMouseButtonPressed(0))
@@ -92,6 +31,7 @@ namespace MohawkGame2D
             }
             //move and draw objects
             objects.MoveAndDraw();
+            player.MoveAndDraw();
         }
     }
 }
---
+++ Player.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Numerics;
+namespace MohawkGame2D
+{
+    public class Player
+    {
+        Vector2 Velocity = new Vector2(0, 0);
+        Vector2 Position = new Vector2(100, 100);
+        int Size = 25;
+        int Speed = 50;
+        int JumpForce = 250;
+        int Gravity = 10;
+            public void MoveAndDraw()
+	    {
+            //player parameters
+            Draw.FillColor = Color.White;
+            Draw.Square(Position, Size);
+
+            //velocity to movement
+            Position += Velocity * Time.DeltaTime;
+
+            //left boundary
+            if (Position.X < 0)
+            {
+                //wall bouncyness left
+                Velocity.X += 50;
+            }
+            else
+            {
+                //left movement
+                if (Input.IsKeyboardKeyDown(KeyboardInput.A))
+                {
+                    Velocity.X -= Speed;
+                }
+            }
+            //right boundary
+            if (Position.X > Window.Size.X - Size)
+            {
+                //wall bouncyness right
+                Velocity.X -= 50;
+            }
+            else
+            {
+                //right movemnt
+                if (Input.IsKeyboardKeyDown(KeyboardInput.D))
+                {
+                    Velocity.X += Speed;
+                }
+            }
+            //floor boundary
+            if (Position.Y < Window.Size.Y - Size)
+            {
+                //gravity
+                Velocity.Y += Gravity;
+            }
+            else
+            {
+                //stop on floor
+                Velocity.Y = 0;
+                Position.Y = Window.Size.Y - Size;
+                //jump
+                if (Input.IsKeyboardKeyPressed(KeyboardInput.W))
+                {
+                    Velocity.Y -= JumpForce;
+                }
+            }
+            //drag X axis
+            Velocity.X += (-Velocity.X * 0.2f);
+
+        }
+    }
+}
+
